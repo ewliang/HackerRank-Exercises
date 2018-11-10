bdf318d16348c374f134d7681b975c24ddc5ed3a
Eric Liang <ewliang@users.noreply.github.com>
2018-11-09 17:25:18 -0800

    Create compare-the-triplets.js

---
+++ JavaScript/compare the triplets/compare-the-triplets.js
@@ -0,0 +1,13 @@
+// SOLUTION
+
+function compareTriplets(a, b) {
+    var score = [0,0];
+    for(let i = 0; i < a.length; i++) {
+        if(a[i] > b[i]) {
+            score[0]++;
+        } else if(a[i] < b[i]) {
+            score[1]++;
+        }
+    }
+    return score;
+}
